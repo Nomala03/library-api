4843e2c07d537e74376fe5cea90f4c0e98f1f73c
nomala03 <mbalimadonsela03@gmail.com>
2025-09-10 18:03:34 +0200

    feat: created author & Book  model & in-memory array

---
+++ src/models.ts
@@ -0,0 +1,34 @@
+import { nanoid } from "nanoid";
+
+export type ID = string;
+
+export interface Author {
+  id: ID;
+  name: string;
+  bio?: string;
+  birthDate?: string;
+}
+
+export interface Book {
+  id: ID;
+  title: string;
+  authorId: ID;
+  publishedAt?: string; 
+  pages?: number;
+  summary?: string;
+  year?: number; // convenience (extracted from publishedAt if present)
+}
+
+
+// In-memory stores
+ 
+export const authors: Author[] = [];
+export const books: Book[] = [];
+
+/** helper to create ids when creating sample/demo data */
+export function createAuthor(data: Omit<Author, "id">): Author {
+  return { id: nanoid(), ...data };
+}
+export function createBook(data: Omit<Book, "id">): Book {
+  return { id: nanoid(), ...data };
+}
